3f680c96f095bc0fe0d3644ff50ff5335a6cd5ac
David Castelblanco Benavides <cbdavides@gmail.com>
2018-11-06 22:10:57 -0500

    Print logic was decoupled from the tree function.

--- src/github/query.py
+++ src/github/query.py
@@ -4,7 +4,17 @@ PIPE = '|'
 BRANCH = '__'
 SEPARATOR = ' '
 
-def tree(repository_url, auth=None, times=0):
+def beautify(name, times, full_path):
+    prfx = ""
+    if times > 0:
+        prfx = "{}{}{} ".format(times*SEPARATOR, PIPE, BRANCH)
+
+    return "{}{}".format(prfx, name)
+
+def identity(name, times, full_path):
+    return full_path
+
+def tree(repository_url, callback, auth=None, times=0):
     """
         This function traverses the file directory of a repository hosted on,
         github and returns a generator with its contents.
@@ -16,20 +26,19 @@ def tree(repository_url, auth=None, times=0):
         r = requests.get( repository_url )
 
     for item in r.json():
-        prfx = ""
-        if times > 0:
-            prfx = "{}{}{} ".format(times*SEPARATOR, PIPE, BRANCH)
 
-        yield "{}{}".format(prfx, item.get('name'))
+        yield callback(item.get('name'), times, item.get('path'))
 
         if(item.get('type') == 'dir'):
-            subtree = tree(repository_url + '/' + item.get('name'), auth, times + 2 )
+            new_url = "{}/{}".format(repository_url, item.get('name'))
+            subtree = tree(new_url, callback, auth, times + 2 )
 
             for branch in subtree:
                 yield branch
 
 
 if __name__ == '__main__':
-    tr = tree('https://api.github.com/repos/SuprDewd/CompetitiveProgramming/contents')
+    url = 'https://api.github.com/repos/SuprDewd/CompetitiveProgramming/contents'
+    tr = tree(url, beautify)
     for t in tr:
         print( t )
