51e516d16c569c6856626047f42dfa73e869196d
David Castelblanco Benavides <cbdavides@gmail.com>
2018-12-13 00:54:05 -0500

    Implemented method to find repositories by user, trying out the new decorator implementation.

--- src/model/dao.py
+++ src/model/dao.py
@@ -2,16 +2,23 @@
 from entity import Repository
 from decorators import get
 
+
 class RepositoryDao:
 
     @classmethod
     @get('SELECT * FROM repository')
-    def get_repositories(self, repostories):
-        for repository in repostories:
+    def getRepositories(**kwargs):
+        for repository in kwargs.get('result', []):
+            yield Repository(repository[0], repository[1])
+
+    @classmethod
+    @get('SELECT * FROM repository WHERE user=?')
+    def getRepositoriesByUser(**kwargs):
+        for repository in kwargs.get('result', []):
             yield Repository(repository[0], repository[1])
 
 
 if __name__ == '__main__':
 
-    for r in RepositoryDao.get_repositories():
+    for r in RepositoryDao.getRepositoriesByUser(params=('cbdavide',)):
         print(r)
