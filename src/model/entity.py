530a3ba417fed5c345cf0fafdc3977eae3496cb9
David Castelblanco Benavides <cbdavides@gmail.com>
2018-12-13 23:45:51 -0500

    Using @classmethod, to reuse code

--- src/model/entity.py
+++ src/model/entity.py
@@ -5,5 +5,10 @@ class Repository:
         self.user = user
         self.name = name
 
+    @classmethod
+    def fromList(cls, repositories):
+        for user, name in repositories:
+            yield cls(user, name)
+
     def __str__(self):
         return "{}/{}".format(self.user, self.name)
